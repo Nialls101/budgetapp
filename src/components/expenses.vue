5c4983d535151b7cc43694bb5a656924a7a6ecea
Neil Bolton <neil.bolton@vml.com>
2019-05-15 15:52:32 +0200

    Add Expense totals rows and Fixed costs to Expenses tab

--- src/components/expenses.vue
+++ src/components/expenses.vue
@@ -1,8 +1,20 @@
 <template>
     <div id="expenses">
       <SortableList lockAxis="y" v-model="items">
-        <SortableItem v-for="(item, index) in items" :index="index" :key="index" :item="item.title" :amount="item.amount"/>
+        <SortableItem v-for="(item, index) in items" :index="index" :key="index" :item="item.title" :amount="item.amount" :fixedCost="item.fixedCost"/>
       </SortableList>
+      <div id="totalExpenses" class="columns">
+        <div class="column col-12 col-xs-12 totals-row">
+          <span class="list-item--title">Total Expenses</span>
+          <span class="list-item--amount">R {{ expenseAmount }}</span>
+        </div>
+      </div>
+      <div id="fixedCosts" class="columns">
+        <div class="column col-12 col-xs-12 totals-row">
+          <span class="list-item--title fixed-cost-text">Fixed Costs</span>
+          <span class="list-item--amount">R {{ fixedCosts }}</span>
+        </div>
+      </div>
     </div>
 </template>
 <script>
@@ -18,11 +30,11 @@ const SortableList = {
 
 const SortableItem = {
   mixins: [ElementMixin],
-  props: ['item', 'amount'],
+  props: ['item', 'amount', 'fixedCost'],
   template: `
     <li class="list-item">
       <span class="list-item--title">
-        <span class="handle"></span> {{item}}</span>
+        <span class="handle"></span> {{item}} <span class="fixed-cost-text" v-if="fixedCost">&nbsp;(FC)</span></span>
       </span>
       <span class="list-item--amount">
         R{{amount}}
@@ -48,16 +60,17 @@ export default {
     },
     data() {
       return {
+        expenseAmount: '0.00',
+        fixedCosts: '0.00',
         newExpenseTitle: '',
         newExpenseAmount: '',
         newExpenseFixedCost: Boolean,
         items: [
           {id: '1', title: 'Expense 1', amount: '50.00', fixedCost: true},
-          {id: '2', title: 'Expense 2', amount: '50.00', fixedCost: true},
+          {id: '2', title: 'Expense 2', amount: '50.00', fixedCost: false},
           {id: '3', title: 'Expense 3', amount: '50.00', fixedCost: true},
-          {id: '4', title: 'Expense 4', amount: '50.00', fixedCost: true}
+          {id: '4', title: 'Expense 4', amount: '50.00', fixedCost: false}
         ],
-        itemsOld: ['Expense 1', 'Expense 2', 'Expense 3', 'Expense 4'],
       };
     },
     methods: {
@@ -69,12 +82,36 @@ export default {
         fixedCost: this.newExpenseFixedCost
       })
       this.newExpenseTitle = ''
+    },
+    totalExpenses: function () {
     }
-    }
+  }
 }
 </script>
 <style scoped>
 li {
   margin-top: 0;
+}
+
+#totalExpenses {
+  width: 100%;
+  max-width: 360px;
+  margin: 0 auto;
+  background: #fff;
+  box-sizing: border-box;
+  border-bottom: 1px solid #efefef;
+}
+#fixedCosts {
+  width: 100%;
+  max-width: 360px;
+  margin: 0 auto;
+  background: #fff;
+  box-sizing: border-box;
+}
+#totalExpenses .list-item--title {
+  padding-left: 46px;
+}
+#fixedCosts .list-item--title {
+  padding-left: 46px;
 }
 </style>
