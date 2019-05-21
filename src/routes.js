bb5e4d309fcbf82b9789fd6ab940b2cb8b253229
Neil Bolton <neil.bolton@vml.com>
2019-05-21 10:15:29 +0200

    Setup routes to Add and Show Expenses, Income and Savings.

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import addExpense from './components/addExpense.vue';
+import showExpenses from './components/showExpenses.vue';
+import addIncome from './components/addIncome.vue';
+import showIncome from './components/showIncome.vue';
+import addSavings from './components/addSavings.vue';
+import showSavings from './components/showSavings.vue';
+import showBudgetSummary from './components/showBudgetSummary.vue';
+
+export default [
+    { path: '/', component: showBudgetSummary},
+    { path: '/expenses', component: showBlogs},
+    { path: '/add-expense', component: addExpense},
+    { path: '/income', component: showIncome},
+    { path: '/add-income', component: addIncome},
+    { path: '/savings', component: showSavings},
+    { path: '/add-savings', component: addSavings},
+]
